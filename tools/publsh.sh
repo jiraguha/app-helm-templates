c7054cf5fe2bf94496a9cd6e0cafe8559244dfc0
jpiraguha <jiraguha@gmail.com>
2022-02-07 11:03:26 +0100

    Revert "fix: services-dependency name"
    This reverts commit 191438740b7612b218dcb3aeac509e08d8a5a1b0.

--- tools/publsh.sh
+++ tools/publsh.sh
@@ -31,12 +31,14 @@ push(){
     rm *.tgz
     cd "$currentDir"
 }
+pushMaster hello
 pushMaster dns
 pushMaster spring-boot
 pushMaster services-dependency
 
-export CI_COMMIT_REF_SLUG=0.5.1
+export CI_COMMIT_REF_SLUG=0.5.0
 
+pushTag hello
 pushTag dns
 pushTag spring-boot
 pushTag services-dependency
